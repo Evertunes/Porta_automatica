d44e7aefa463220c18d8f0e32d5efcf9ac8fd13f
Everton Alves <alveseverton023@gmail.com>
2022-06-13 19:51:58 -0300

    finalizado

---
+++ porta.js
@@ -0,0 +1,40 @@
+let sense1 = true;
+let sense2 = true;
+let sense3 = true;
+let sense4 = true;
+
+function condic(sense1, sense2, sense3, sense4) {
+
+if (sense1 == true && sense2 == true && sense3 == true && sense4 == true)
+{
+  let result = "Portas Liberadas"
+  console.log(result);
+
+}
+else{
+
+    let result = "Acesso Negado"
+  console.log(result);
+
+}
+
+return;
+
+}
+
+condic(sense1,sense2,sense3,sense4);
+
+
+switch (condic) {
+
+case false:
+  console.log(result2);
+break;
+
+case true:
+console.log (result);  
+break;
+}
+
+
+
