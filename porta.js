d0783db9cfbcbf7b45699448d8c59a81266dd897
Everton Alves <alveseverton023@gmail.com>
2022-06-13 21:05:27 -0300

    formatação de documento

--- porta.js
+++ porta.js
@@ -4,24 +4,15 @@ let sense3 = true;
 let sense4 = true;
 
 function condic(sense1, sense2, sense3, sense4) {
+  if (sense1 == true && sense2 == true && sense3 == true && sense4 == true) {
+    let result = "Portas Liberadas";
+    console.log(result);
+  } else {
+    let result = "Acesso Negado";
+    console.log(result);
+  }
 
-if (sense1 == true && sense2 == true && sense3 == true && sense4 == true)
-{
-  let result = "Portas Liberadas"
-  console.log(result);
-
-}
-else{
-
-    let result = "Acesso Negado"
-  console.log(result);
-
+  return;
 }
 
-return;
-
-}
-
-condic(sense1,sense2,sense3,sense4);
-
-
+condic(sense1, sense2, sense3, sense4);
